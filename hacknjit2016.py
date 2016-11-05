0748ed42240c6c4c5d428cdc9fb2ecb2e7af618f
jm672 <jm672@njit.edu>
2016-11-05 12:12:32 -0400

    Update hacknjit2016.py

--- hacknjit2016.py
+++ hacknjit2016.py
@@ -1,1 +1,19 @@
+import random
 wordBank = ['amalgamation','gathering','hilarious','particular','representations','sodiumcholrine','carbohydrate']
+stat = true
+strike = 0
+score = 0
+print("Type the word then press enter!")
+while stat == true:
+  wordQuiz = wordBank[random.randint(0,len(wordBank))]
+  wordType = input(wordQuiz)
+  if wordType == wordQuiz:
+    score += 1
+    continue
+  else:
+    strike += 1
+  if strike == 3:
+    print('Game Over!')
+    break
+  
+  
